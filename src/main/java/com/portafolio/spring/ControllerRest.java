ae770a4cedc87a58d8ca217f19570eac3df38bc1
Marcos Imac <marcos737273@hotmail.com>
2023-08-12 11:12:00 -0600

    Summary

--- src/main/java/com/portafolio/spring/ControllerRest.java
+++ src/main/java/com/portafolio/spring/ControllerRest.java
@@ -1,13 +1,15 @@
-
 package com.portafolio.spring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@Slf4j
 public class ControllerRest {
     @GetMapping("/")
     public String start(){
-        return"Portafolio";
+        log.info("Controller MVC - return ControllerRest");
+        return"index";
     }
 }
